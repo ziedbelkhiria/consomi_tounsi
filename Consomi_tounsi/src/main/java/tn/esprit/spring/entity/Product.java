a6846ba713859ac5d040adee2900ce725d7a47b6
siwar20 <siwar.berzouga@esprit.tn>
2021-03-17 22:58:59 +0100

    Rename entity Product

---
+++ Consomi_tounsi/src/main/java/tn/esprit/spring/entity/Product.java
@@ -0,0 +1,67 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_PRODUCTS")
+public class Product implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue (strategy= GenerationType.IDENTITY)
+	@Column(name="product_id")
+	private int id;
+	private String product_type;
+	private String product_name ;
+	private String product_category ;
+	private double product_price ;
+	
+	@ManyToOne
+	Category category;
+	
+	@ManyToOne
+	Stocks stocks;
+	
+
+	@ManyToOne
+	Provider provider;
+	
+	
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_category() {
+		return product_category;
+	}
+	public void setProduct_category(String product_category) {
+		this.product_category = product_category;
+	}
+	public double getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(double product_price) {
+		this.product_price = product_price;
+	}
+
+}
