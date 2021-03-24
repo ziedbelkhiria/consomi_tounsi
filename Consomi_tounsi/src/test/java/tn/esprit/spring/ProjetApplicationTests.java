c093fb7fd7a3384df4296249874218e8c9639eb8
siwar20 <siwar.berzouga@esprit.tn>
2021-03-24 16:00:53 +0100

    Modifier ProjetApplicationTests

--- Consomi_tounsi/src/test/java/tn/esprit/spring/ProjetApplicationTests.java
+++ Consomi_tounsi/src/test/java/tn/esprit/spring/ProjetApplicationTests.java
@@ -1,24 +1,80 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
+//import java.util.List;
+
+//import java.util.Date;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//import tn.esprit.spring.entity.Products;
-import tn.esprit.spring.service.ProductService;
+
+//import tn.esprit.spring.entity.Category;
+
+//import tn.esprit.spring.repository.CategoryRepository;
+//import tn.esprit.spring.repository.ProductRepository;
+//import tn.esprit.spring.repository.StockRepository;
+import tn.esprit.spring.service.stockService;
+import tn.esprit.spring.entity.Stocks;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjetApplicationTests {
 	
 	@Autowired
-	ProductService proservice;
+	stockService proRep;
 
 	@Test
-	void contextLoads() {
-		//Products P = new Products(1,"" )
+	public void testAddStocks() {
+		Stocks S =new Stocks(1,100);
+		proRep.addStocks(S);
 	}
+	
+	/*@Test
+	public void retriveStocks() {
+		Stocks S =proRep.retrieveStocks(5).get();
+		System.out.println(S);
+	
+	
+	}
+	@Test
+	public void updateStocks() {
+		if (Stocks.getId() != null) {
+			proRep.updateStocks(1);
+		System.out.println(S);
+	}*/
+	@Test
+	public void deleteStocks() {
+		proRep.deleteStocks(1L);
+		
+	}
+	@Test
+	public void retriveALLStocks() {
+		
+		for(Stocks S: proRep.retrieveAllStocks()){
+			
+			System.out.println(S);
+		}
+	}
+	
+
+
+
+
 
 }
+
+			
+		
+
+	
+	
+	
+	
+
+
